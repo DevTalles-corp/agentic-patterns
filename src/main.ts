ae434619d7e72a7da93f0428f6a87775f372928f
Fernando Herrera <fernando.herrera85@gmail.com>
2026-08-13 10:07:32 -0400

    fix: regresar a la función de pruebas

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import './helpers/string-colors.js';
-// import {
-//   getMessageFromModel,
-//   getMessageFromModelFailSafe,
-//   getMessageFromModelWithTrace,
-// } from './actions/get-message-model.js';
-// import { toolUseMain } from './patterns/01-tool-use/tool-use.js';
-// import { planningMain } from './patterns/02-planning/planning-pattern.js';
-import { reflectionMain } from './patterns/03-reflection/reflection-pattern.js';
+import { getMessageFromModel } from './actions/get-message-model.js';
 
 console.clear();
 
-// await getMessageFromModel();
-// await getMessageFromModelFailSafe();
-// await toolUseMain();
-// await planningMain();
-
-await reflectionMain();
+await getMessageFromModel();
