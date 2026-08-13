8347074b0e747ebefde538b89d04cec73c0e12ae
Fernando Herrera <fernando.herrera85@gmail.com>
2026-08-13 09:11:43 -0400

    Primer commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './helpers/string-colors.js';
+// import {
+//   getMessageFromModel,
+//   getMessageFromModelFailSafe,
+//   getMessageFromModelWithTrace,
+// } from './actions/get-message-model.js';
+// import { toolUseMain } from './patterns/01-tool-use/tool-use.js';
+// import { planningMain } from './patterns/02-planning/planning-pattern.js';
+import { reflectionMain } from './patterns/03-reflection/reflection-pattern.js';
+
+console.clear();
+
+// await getMessageFromModel();
+// await getMessageFromModelFailSafe();
+// await toolUseMain();
+// await planningMain();
+
+await reflectionMain();
